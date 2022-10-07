69b0a22e77c814948638b6b14e897af504e7dd23
Esther Simon <93126974+just-talis@users.noreply.github.com>
2022-10-07 16:44:34 +0000

    class

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import './App.css'
  function App() {
   return (
     <div className='calculator-grid'>
+      <div className='output'>
+        <div className='previous-operand'></div>
+       <div className='previous-operand'></div>
+      </div>
     </div>
   )
 }
